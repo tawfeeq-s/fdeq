9b1dd9c594288e67d779b57af02c86b5f7041a41
Developer Platform <>
2024-01-28 10:20:06 +0000

    Init Upload

---
+++ src/main/java/adsfasdf/service/SafdaSadfsadfService.java
@@ -0,0 +1,13 @@
+package adsfasdf.service;
+
+
+
+ 
+     
+     
+     
+
+
+public interface SafdaSadfsadfService {
+   
+}
---
+++ src/main/java/adsfasdf/service/SafdaSadfsadfServiceImpl.java
@@ -0,0 +1,15 @@
+package adsfasdf.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+ 
+   
+   
+   
+
+
+
+@ApplicationScoped
+public class SafdaSadfsadfServiceImpl implements SafdaSadfsadfService{
+   
+    
+}  
